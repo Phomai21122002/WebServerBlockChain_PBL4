fef1531f10e81fdf9568b849d9bb1e76c24c4194
PhoMai <thanhmai.20570@gmail.com>
2022-10-29 22:04:32 +0700

    thay doi productDetail

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,6 +3,7 @@ const productController = require('../app/controllers/productController')
 const express = require('express');
 const router = express.Router();
 
+router.get('/productDetail', productController.productDetail)
 router.get('/', productController.index)
 
 module.exports = router;
