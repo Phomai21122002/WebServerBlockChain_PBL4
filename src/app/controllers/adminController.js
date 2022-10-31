434164238722bde63f034b4b2202e4d77f6a502a
= <=buithiennhan0345@gmail.com>
2022-10-31 13:11:11 +0700

    update 31/10/2022

--- src/app/controllers/adminController.js
+++ src/app/controllers/adminController.js
@@ -1,9 +1,81 @@
+const request = require('request')
+
 class adminController { 
 
-    //GET /news
+    //GET /admin
     index(req, res){
         res.render('admin/adminPage.hbs', {
             layout: 'adminLayout'
+        })
+    }
+
+    //GET /admin/danhsachttkd
+    danhsachttkd(req,response){
+
+        fetch('http://localhost:3000/api/ttkiemdinh')
+            .then(res => {
+                if(res.ok){
+                    return res.json()
+                }
+                else{
+                    return response.render('error/error500.hbs', {
+                        layout: false
+                    })
+                }
+            })
+            .then((data) => {
+                response.render('admin/listCenter.hbs', {
+                    layout: 'adminLayout',
+                    data: data
+                })
+            })
+            .catch((err) => {
+                return response.render('error/error500.hbs', {
+                    layout: false
+                })
+            })
+
+    }
+
+    danhsachdoanhnghiep(req, response)
+    {
+        response.render('admin/listBusiness.hbs',{
+            layout: 'adminLayout.hbs'
+        })
+    }
+
+    themttkd(req,res){
+        res.render('admin/themttkd.hbs',{
+            layout: 'adminLayout.hbs'
+        })
+    }
+
+    themdoanhnghiep(req,res){
+        res.render('admin/themdoanhnghiep.hbs',{
+            layout: 'adminLayout.hbs'
+        })
+    }
+
+    // POST admin/addttkd
+    addttkd(req,response)
+    {
+        const userName = req.body.userName
+        const email = req.body.email
+        const password = req.body.password
+        const confirmPassword = req.body.confirmPassword
+        const centerName = req.body.centerName
+        const address = req.body.address
+        const phoneNumber = req.body.phone
+
+
+        response.send({
+            userName: userName,
+            email: email,
+            password: password,
+            confirmPassword: confirmPassword,
+            centerName: centerName,
+            address: address,
+            phoneNumbe: phoneNumber,
         })
     }
     
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,5 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', adminController.index)
+router.get('/danhsachttkd',adminController.danhsachttkd)
+router.get('/danhsachdoanhnghiep',adminController.danhsachdoanhnghiep)
+router.get('/themttkd',adminController.themttkd)
+router.get('/themdoanhnghiep',adminController.themdoanhnghiep)
+
+router.post('/addttkd',adminController.addttkd)
 
 module.exports = router;
