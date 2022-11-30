62ad76d5f8390c031b2df50cedb2568881244668
nhanbui1512 <buithiennhan0345@gmail.com>
2022-11-30 23:47:05 +0700

    update 30.11.2022

--- src/app/controllers/centerController.js
+++ src/app/controllers/centerController.js
@@ -1,8 +1,79 @@
+const {multipleMongooseToObject} = require ('../../util/mongoose')
+const {mongooseToObject} = require('../../util/mongoose')
+
+const application = require('../models/application')
+const product = require('../models/sanpham')
+const material = require('../models/nguyenlieu')
+const user = require('../models/user')
+
 
 class centerController{
     index(req, res)
     {
-        res.send('Center Page')
+        res.render('center/dashboardPage.hbs',{layout: 'centerLayout.hbs'})
+    }
+    profile(req, res){
+        var userID = req.session.userid
+        user.findById(userID, (err,data) => {
+            if(!err){
+                res.render('center/profile.hbs', {
+                    layout: 'centerLayout.hbs',
+                    user: mongooseToObject(data)
+                })
+
+            }
+        })
+
+    }
+    listApplication(req,res){
+        application.find({}, (err, data) => {
+            if(!err){
+                res.render('center/listApplication.hbs', {
+                    layout: 'centerLayout.hbs',
+                    data: multipleMongooseToObject(data)
+                })
+            }
+        })
+    }
+    listProduct(req,res){
+
+        product.find({}, function (err, sanphams) {
+            if(err){
+                console.log(err)
+            }
+            else{
+                res.render('admin/listProduct.hbs',{
+                    layout: 'centerLayout.hbs',
+                    data: multipleMongooseToObject(sanphams)
+                })
+            }
+        })
+    }
+
+    listMaterial(req,res){
+        material.find({}, (err, data) => {
+            if(!err){
+                res.render('center/listMaterial.hbs', {
+                    layout: 'centerLayout.hbs',
+                    data: multipleMongooseToObject(data)
+                })
+            }
+        })
+    }
+
+    listBusiness(req,res){
+        user.find({Quyen: 2}, (err, data) => {
+            if(!err){
+                res.render('center/listBusiness.hbs', {
+                    layout: 'centerLayout.hbs',
+                    data: multipleMongooseToObject(data)
+                })
+            }
+        } )
+    }
+
+    productDetail(req,res){
+        
     }
 }
 
--- src/routes/center.js
+++ src/routes/center.js
@@ -5,5 +5,11 @@ const centerController = require('../app/controllers/centerController');
 const router = express.Router();
 
 router.get('/', centerController.index)
+router.get('/profile',centerController.profile)
+router.get('/listapplication',centerController.listApplication)
+router.get('/listproduct',centerController.listProduct)
+router.get('/listmaterial',centerController.listMaterial)
+router.get('/listbusiness',centerController.listBusiness)
+
 
 module.exports = router;
